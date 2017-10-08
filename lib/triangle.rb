7246b032d162f6857698b257bffd92dd574de420
Aaron Samuel <aasamuelj@gmail.com>
2017-10-08 04:29:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,11 +15,20 @@ class Triangle
     @c = c
   end
   def kind
-      @a, @b, @c = [@a, @b, @c].sort
-      raise TriangleError if @a <= 0 or @a + @b <= @c
-      return :equilateral if @a == @c
-      return :isosceles if @a == @b or @b == @c
-      return :scalene
+      @a, @b, @c = [@a, @b, @c].sort #=> lower to higher
+      raise TriangleError if @a <= 0 or @a + @b <= @c #=> a has to be greater than 0, since c is the largest number, the sum
+      #=> of a and b
+      # return :equilateral if @a == @c
+      # return :isosceles if @a == @b or @b == @c
+      # return :scalene
+      case self 
+      when self.a == self.c 
+        :equilateral
+      when self.a == self.b or self.b == self.c
+        :isosceles
+      else 
+        :scalene
+      end
     end
   end
 
